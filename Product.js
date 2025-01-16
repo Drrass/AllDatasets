bdc9b240534794fb904e38043f0c8231ee92946f
Himanshu Mehra <100959929+Drrass@users.noreply.github.com>
2025-01-16 16:46:34 +0530

    Create Product.js

---
+++ Product.js
@@ -0,0 +1,29 @@
+const products = [{
+    shampoo: {
+        price: "$50",
+        quantity: 4
+    },
+    "Hair-oil": {
+        price: "$40",
+        quantity: 2,
+        sealed: true
+    },
+    comb: {
+        price: "$12",
+        quantity: 1
+    },
+    utensils: [
+        {
+            spoons: { quantity: 2, price: "$8" }
+        }, {
+            glasses: { quantity: 1, price: "$70", type: "fragile" }
+        },{
+            cooker: { quantity: 4, price: "$900" }
+        }
+    ],
+    watch: {
+        price: "$800",
+        quantity: 1,
+        type: "fragile"
+    }
+}]
