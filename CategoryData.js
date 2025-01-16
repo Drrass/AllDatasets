cadd74cbc8b6f976ee41d40525d5c54f12f68f9b
Himanshu Mehra <100959929+Drrass@users.noreply.github.com>
2025-01-16 16:49:52 +0530

    Create CategoryData.js

---
+++ CategoryData.js
@@ -0,0 +1,22 @@
+const data = [
+  {
+    price: "$30",
+    category: "electronics",
+  },
+  {
+    price: "$38",
+    category: "apparel",
+  },
+  {
+    price: "$321",
+    category: "furniture",
+  },
+  {
+    price: "$300",
+    category: "electronics",
+  },
+  {
+    price: "$90.5",
+    category: "electronics",
+  },
+];
