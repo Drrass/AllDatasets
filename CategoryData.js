b6ea0deb8eb3823bbccbf5ba2399bb0061b9f2d0
Himanshu Mehra <100959929+Drrass@users.noreply.github.com>
2025-01-22 15:30:02 +0530

    Update CategoryData.js

--- CategoryData.js
+++ CategoryData.js
@@ -1,22 +1,33 @@
-const data = [
-  {
-    price: "$30",
-    category: "electronics",
-  },
-  {
-    price: "$38",
-    category: "apparel",
-  },
-  {
-    price: "$321",
-    category: "furniture",
-  },
-  {
-    price: "$300",
-    category: "electronics",
-  },
-  {
-    price: "$90.5",
-    category: "electronics",
-  },
-];
+const items = [
+    {
+        'item': 'mouse pad',
+        'price': '$30',
+        'category': 'electronics'
+    },
+    {
+        'item': 'tshirt',
+        'price': '$38',
+        'category': 'apparel'
+    },
+    {
+        'item': 'table',
+        'price': '$321',
+        'category': 'furniture'
+    },
+    {
+        'item': 'keyboard',
+        'price': '$300',
+        'category': 'electronics'
+    },
+    {
+        'item': 'charging cable'
+        'price': '$90.5',
+        'category': 'electronics'
+    },
+    {
+        'item: 'adapter',
+        'price': '$100.5',
+        'category': 'electronics'
+    },
+
+] 
